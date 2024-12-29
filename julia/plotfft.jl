ff97c1b6525cc9dc55ace9371f0d3351003c215c
Brooks J Rady <b.j.rady@gmail.com>
2024-12-28 19:37:44 -0700

    Initial Commit

---
+++ julia/plotfft.jl
@@ -0,0 +1,28 @@
+using Plots
+using FFTW
+
+# Sample period
+Ts = 1 / (44140)
+
+function plotfft(signal)
+    # Number of points 
+    N = length(signal)
+    # Time coordinate
+    t = range(0, step=Ts, length=N)
+
+    # Fourier Transform of it 
+    F = fft(signal) |> fftshift
+    freqs = fftfreq(length(t), 1.0/Ts) |> fftshift
+
+    # Plotting
+    ms = 1000t
+    time_domain = plot(ms, signal, title = "Signal")
+    freq_domain = plot(freqs, abs.(F), title = "Spectrum", xlim=(0, +5000)) 
+    plot(time_domain, freq_domain, layout = 2)
+end
+
+function stereoplot(left, right)
+    left = plotfft(left .- 126)
+    right = plotfft(right .- 126)
+    plot(left, right, layout = (2, 1))
+end
